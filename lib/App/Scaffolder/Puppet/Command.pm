e171413b44ea2a8e545f1e43cd96e30798bb6b97
Manfred Stock <mstock@cpan.org>
2015-02-23 15:47:26 +0100

    Extract App::Scaffolder::Puppet::Command command base class, add test base class

---
+++ lib/App/Scaffolder/Puppet/Command.pm
@@ -0,0 +1,148 @@
+package App::Scaffolder::Puppet::Command;
+use parent qw(App::Scaffolder::Command);
+
+# ABSTRACT: Base class for App::Scaffolder::Puppet commands
+
+use strict;
+use warnings;
+
+use File::Spec::Functions qw(catfile);
+use MRO::Compat;
+use Path::Class::Dir;
+
+=head1 SYNOPSIS
+
+	use parent qw(App::Scaffolder::Puppet::Command);
+
+=head1 DESCRIPTION
+
+This class provides some specialized methods for the C<puppet*> commands.
+
+In addition to the default template search path (see
+L<App::Scaffolder|App::Scaffolder> for details), this command base class will
+also look for templates in C</etc/puppet/scaffolder_templates> or
+C</usr/local/etc/puppet/scaffolder_templates> if they exist.
+
+=head1 METHODS
+
+=head2 get_target
+
+Specialized C<get_target> version which uses the name (if it does not contain
+C<::>, otherwise, it will be the current working directory) if no target was
+given.
+
+=cut
+
+sub get_target {
+	my ($self, $opt) = @_;
+	my $target = $opt->target() || (
+		$opt->name() =~ m{::}x
+			? '.'
+			: $opt->name()
+	);
+	return Path::Class::Dir->new($target);
+}
+
+
+=head2 get_variables
+
+Specialized C<get_variables> version which returns the name of the module and
+other useful variables.
+
+=cut
+
+sub get_variables {
+	my ($self, $opt) = @_;
+
+	my @name_parts = split(/::/x, $opt->name());
+	my (undef, @subname_parts) = @name_parts;
+	my $package = $opt->package() || $opt->name();
+	return {
+		name               => scalar $opt->name(),
+		nameparts          => \@name_parts,
+		namepartsjoined    => join('_', @name_parts),
+		namepartspath      => catfile(@name_parts),
+		subnameparts       => \@subname_parts,
+		subnamepartsjoined => join('_', @subname_parts),
+		subnamepartspath   => catfile(@subname_parts),
+		package            => $package,
+	};
+}
+
+
+=head2 get_dist_name
+
+Return the name of the dist this command is in.
+
+=cut
+
+sub get_dist_name {
+	return 'App-Scaffolder-Puppet';
+}
+
+
+=head2 get_options
+
+Return additional options for this command.
+
+=cut
+
+sub get_options {
+	my ($class) = @_;
+	return (
+		[ 'name|n=s'    => 'Name of the new Puppet module/class that should be created' ],
+		[ 'package|p=s' => 'Name of a package that should be available in templates '
+			. '(defaults to the value of the --name parameter)' ],
+	);
+}
+
+
+sub validate_args {
+	my ($self, $opt, $args) = @_;
+
+	$self->next::method($opt, $args);
+	unless ($self->contains_base_args($opt) || $opt->name()) {
+		$self->usage_error("Parameter 'name' required");
+	}
+	return;
+}
+
+
+=head2 get_extra_template_dirs
+
+Extend the template search path with C</etc/puppet/scaffolder_templates> or
+C</usr/local/etc/puppet/scaffolder_templates> if they exist.
+
+=head3 Result
+
+The extended list with template directories.
+
+=cut
+
+sub get_extra_template_dirs {
+	my ($self, $command) = @_;
+
+	my $template_dir_name = 'scaffolder_templates';
+	my @extra_template_dirs = grep { -d $_ && -r $_ } (
+		Path::Class::Dir->new('', 'etc', 'puppet', $template_dir_name),
+		Path::Class::Dir->new('', 'usr', 'local', 'etc', 'puppet', $template_dir_name),
+	);
+
+	return (
+		$self->next::method($command),
+		@extra_template_dirs,
+	);
+}
+
+
+1;
+
+=head1 SEE ALSO
+
+=over
+
+=item *
+
+L<App::Scaffolder::Command|App::Scaffolder::Command>
+
+=back
--- t/lib/App/Scaffolder/CommandTest/puppetmoduleTest.pm
+++ t/lib/App/Scaffolder/CommandTest/puppetmoduleTest.pm
@@ -1,16 +1,13 @@
 package App::Scaffolder::CommandTest::puppetmoduleTest;
-use parent qw(Test::Class);
+use parent qw(App::Scaffolder::Puppet::TestBase);
 
 use strict;
 use warnings;
 
 use Carp;
 use Test::More;
-use Test::Exception;
-use Test::MockObject;
 use App::Cmd::Tester;
 use Path::Class::Dir;
-use File::Spec::Functions qw(catfile);
 use Test::File;
 use Test::File::ShareDir '-share' => {
 	'-dist' => { 'App-Scaffolder-Puppet' => Path::Class::Dir->new(qw(share)) }
@@ -18,25 +15,6 @@ use Test::File::ShareDir '-share' => {
 use Directory::Scratch;
 use App::Scaffolder;
 use App::Scaffolder::Command::puppetmodule;
-
-
-sub setup : Test(setup) {
-	my ($self) = @_;
-
-	$self->{opt_mock} = Test::MockObject->new();
-	delete $self->{target_opt};
-	delete $self->{name_opt};
-	delete $self->{package_opt};
-	$self->{opt_mock}->mock(target => sub {
-		return $self->{target_opt};
-	});
-	$self->{opt_mock}->mock(name => sub {
-		return $self->{name_opt};
-	});
-	$self->{opt_mock}->mock(package => sub {
-		return $self->{package_opt};
-	});
-}
 
 
 sub app_test : Test(7) {
@@ -60,80 +38,5 @@ sub app_test : Test(7) {
 	}
 }
 
-
-sub get_extra_template_dirs_test : Test(2) {
-	my ($self) = @_;
-
-	my $sharedir = Path::Class::Dir->new(qw(share));
-	my $cmd = App::Scaffolder::Command::puppetmodule->new({});
-	local $ENV{SCAFFOLDER_TEMPLATE_PATH} = $sharedir->stringify();
-	my @template_dirs = $cmd->get_extra_template_dirs('puppetmodule');
-	cmp_ok(scalar @template_dirs, '>=', 1, 'at least one template dir found');
-	is(
-		scalar(grep { $_ eq $sharedir->subdir('puppetmodule')->stringify() } @template_dirs),
-		1,
-		'path from environment respected'
-	);
-}
-
-
-sub get_target_test : Test(3) {
-	my ($self) = @_;
-
-	my $cmd = App::Scaffolder::Command::puppetmodule->new({});
-
-	$self->{name_opt} = 'foo';
-	is($cmd->get_target($self->{opt_mock}), 'foo', 'target ok');
-
-	$self->{name_opt} = 'foo::bar';
-	is($cmd->get_target($self->{opt_mock}), '.', 'target ok');
-
-	$self->{target_opt} = 'test/dir';
-	is($cmd->get_target($self->{opt_mock}), 'test/dir', 'target parameter overrides');
-}
-
-
-sub get_variables_test : Test(3) {
-	my ($self) = @_;
-
-	my $cmd = App::Scaffolder::Command::puppetmodule->new({});
-
-	$self->{name_opt} = 'foo';
-	is_deeply($cmd->get_variables($self->{opt_mock}), {
-		name               => 'foo',
-		package            => 'foo',
-		nameparts          => ['foo'],
-		namepartsjoined    => 'foo',
-		namepartspath      => 'foo',
-		subnameparts       => [],
-		subnamepartsjoined => '',
-		subnamepartspath   => undef,
-	}, 'variables ok');
-
-	$self->{name_opt} = 'foo::bar';
-	is_deeply($cmd->get_variables($self->{opt_mock}), {
-		name               => 'foo::bar',
-		package            => 'foo::bar',
-		nameparts          => ['foo', 'bar'],
-		namepartsjoined    => 'foo_bar',
-		namepartspath      => catfile('foo', 'bar'),
-		subnameparts       => ['bar'],
-		subnamepartsjoined => 'bar',
-		subnamepartspath   => 'bar',
-	}, 'variables ok');
-
-	$self->{name_opt} = 'foo::bar::baz';
-	$self->{package_opt} = 'foo-bar-baz';
-	is_deeply($cmd->get_variables($self->{opt_mock}), {
-		name               => 'foo::bar::baz',
-		package            => 'foo-bar-baz',
-		nameparts          => ['foo', 'bar', 'baz'],
-		namepartsjoined    => 'foo_bar_baz',
-		namepartspath      => catfile('foo', 'bar', 'baz'),
-		subnameparts       => ['bar', 'baz'],
-		subnamepartsjoined => 'bar_baz',
-		subnamepartspath   => catfile('bar', 'baz'),
-	}, 'variables ok');
-}
 
 1;
