961cc676e2bc1b807533c8e231bd6f7c9450023b
Manfred Stock <mstock@cpan.org>
2015-02-23 17:00:26 +0100

    Add 'puppetclass' command, move 'subpackage' template to it
    The 'subpackage' template does not create a complete Puppet module, so it
    makes more sense to introduce a 'puppetclass' command which provides access
    to this template.

--- lib/App/Scaffolder/Command/puppetmodule.pm
+++ lib/App/Scaffolder/Command/puppetmodule.pm
@@ -11,14 +11,8 @@ use warnings;
 	# Create scaffold to install the 'vim' package:
 	$ scaffolder puppetmodule --template package --name vim
 
-	# Create scaffold to install the 'vim-puppet' package in module created with above command:
-	$ scaffolder puppetmodule --template subpackage --name vim::puppet --package vim-puppet
-
 	# Create scaffold to install the 'apache2' package and setup the corresponding service:
 	$ scaffolder puppetmodule --template service --name apache2
-
-	# Create scaffold to install the 'apache2-doc' package in module created with above command:
-	$ scaffolder puppetmodule --template subpackage --name apache2::doc --package apache2-doc
 
 =head1 DESCRIPTION
 
@@ -35,15 +29,6 @@ C<package>: Create Puppet module to install a package.
 
 C<service>: Create Puppet module to setup a service.
 
-=item *
-
-C<subpackage>: Create class to install a 'sub package'. This is intended to be
-used after using C<package> or C<service> to add an additional package to the
-module (eg. C<apache2-doc> to the C<apache2> service). This must be used inside
-the module directory created before, and you will have to add a variable with
-the actual package name to the existing C<manifests/params.pp> file. The name of
-the variable can be seen in the newly created files below C<manifests>.
-
 =back
 
 =head1 SEE ALSO
@@ -53,6 +38,10 @@ the variable can be seen in the newly created files below C<manifests>.
 =item *
 
 L<https://docs.puppetlabs.com/puppet/latest/reference/modules_fundamentals.html> - Module Fundamentals
+
+=item *
+
+L<App::Scaffolder::Command::puppetclass|App::Scaffolder::Command::puppetclass>
 
 =back
 
--- lib/App/Scaffolder/Puppet.pm
+++ lib/App/Scaffolder/Puppet.pm
@@ -7,9 +7,14 @@ use warnings;
 
 =head1 DESCRIPTION
 
-App::Scaffolder::Puppet provides a command to scaffold Puppet modules. See
-L<App::Scaffolder::Command::puppetmodule|App::Scaffolder::Command::puppetmodule>
-for the actual command.
+App::Scaffolder::Puppet provides commands to scaffold Puppet modules and classes.
+See L<App::Scaffolder::Command::puppetmodule|App::Scaffolder::Command::puppetmodule>
+and L<App::Scaffolder::Command::puppetclass|App::Scaffolder::Command::puppetclass>
+for the actual commands.
+
+L<App::Scaffolder::Puppet::Command|App::Scaffolder::Puppet::Command> is a base
+class for the above commands, and is itself based on
+L<App::Scaffolder::Command|App::Scaffolder::Command>.
 
 =head1 SEE ALSO
 
@@ -18,6 +23,14 @@ for the actual command.
 =item *
 
 L<App::Scaffolder|App::Scaffolder>
+
+=item *
+
+L<App::Scaffolder::Puppet::Command|App::Scaffolder::Puppet::Command>
+
+=item *
+
+L<https://puppetlabs.com/puppet/puppet-open-source> - Puppet
 
 =back
 
